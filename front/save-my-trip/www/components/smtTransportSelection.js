f40bf5cc74518fa89429bad70caed0e45350c28b
pingouin2k <tnguyen.hung@gmail.com>
2016-11-26 21:00:09 +0100

    transport icons

--- front/save-my-trip/www/components/smtTransportSelection.js
+++ front/save-my-trip/www/components/smtTransportSelection.js
@@ -1,23 +1,38 @@
 angular.module('saveMyTrip')
   .component('smtTransportSelection', {
     'templateUrl': 'components/smtTransportSelection.html',
-    'controller': function ($scope, $state, transportSrv, userSrv) {
+    'controller': function ($scope, $state, transportSrv, userSrv, iconSrv) {
       $ctrl = this;
       $ctrl.userSrv = userSrv;
-      $ctrl.transportSrv = transportSrv;
+      $ctrl.iconSrv = iconSrv;
 
       $ctrl.getTransports = function (options) {
-        $ctrl.transportSrv.get(options)
+        transportSrv.get(options)
           .then(function (res) {
             console.log('get transport choices');
             console.log(res.data);
-            $ctrl.transportSrv.transports = res.data;
+            transportSrv.transports = res.data;
           });
       };
 
       $ctrl.selectTransport = function (transport) {
         userSrv.transportChoice = transport;
         $state.go('app.transportMap');
+      };
+
+      $ctrl.getTransportIndex = function (transportType){
+        if(transportSrv.transports.length === 0){
+          return {};
+        }
+        console.log($ctrl.transportSrv);
+        var val = transportSrv.transports.filter(function(f){
+          return transportType === f.type;
+        });
+        return val[0];
+      };
+
+      $ctrl.hasTransport = function(){
+        return transportSrv.transports.length > 0;
       };
 
       $ctrl.getTransports();
